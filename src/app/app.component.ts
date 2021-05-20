4b1eee6874657fea65680b5ebf02d8a440092f0b
Anartz Mugika Ledo <mugan86@gmail.com>
2021-05-20 12:53:57 +0200

    Finish Angular jsPDF implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
-
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import breakingBad from './fake-data.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-jspdf';
+  breakingBadData = breakingBad;
+  constructor() {
+    // this.downloadPDF();
+    console.log(this.breakingBadData);
+  }
+  public downloadPDF() {
+    // Extraemos el
+    const DATA: any = document.getElementById('htmlData');
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3,
+    };
+    html2canvas(DATA, options)
+      .then((canvas) => {
+        const img = canvas.toDataURL('image/PNG');
+
+        // Add image Canvas to PDF
+        const bufferX = 15;
+        const bufferY = 15;
+        const imgProps = (doc as any).getImageProperties(img);
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        doc.addImage(
+          img,
+          'PNG',
+          bufferX,
+          bufferY,
+          pdfWidth,
+          pdfHeight,
+          undefined,
+          'FAST'
+        );
+        return doc;
+      })
+      .then((docResult) => {
+        docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+      });
+  }
 }
